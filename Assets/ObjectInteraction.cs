ac1e02074c154d08b66e46e837c8e10cb647053c
João Freitas <joao.david.freitas@gmail.com>
2020-05-27 17:51:20 +0100

    Project Commit

---
+++ Assets/ObjectInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputSource();
+    }
+
+    void CheckInputSource()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.Any, OVRInput.Controller.LTrackedRemote))
+        {
+            Debug.Log("Left hand");
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Any, OVRInput.Controller.RTrackedRemote))
+        {
+            Debug.Log("Right hand");
+        }
+    }
+}
